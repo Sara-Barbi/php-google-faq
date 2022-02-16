70950ed62f57831511914493702bcf91961fd7b1
Sara-Barbi <barbisara02@gmail.com>
2022-02-16 16:04:34 +0100

    inizio

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+$arr=[
+    'questions'=> [
+        'title' => 'mammmmellousi',
+        'domDue' => 'aaah'
+    ],
+    'response' => [
+        'title' => 'minchia!',
+        'respDue' => 'aooh'
+
+    ]
+    ];
+
+foreach($arr as $data => $element){
+  //    echo $element['title'];
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">   <!--Bootstrap.css--> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">      <!--Bootstrap ICONS-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />    <!--Font AWESOME ICONS-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Zen+Kaku+Gothic+Antique:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row  my_heig ">
+            <div class="col-12 d-flex align-items-center position-relative">
+                <img class="google_logo" src="./img/google.png" alt="">
+                <div class="privacy_title ms-2">Privacy e Termini</div>
+                <div class="my-p"><i class="fas fa-th"></i></div>
+                <img class="utent_img" src="./img/uomo.jpg" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <ul class="d-flex">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+
+            </div>
+        </div>
+    </div>  
+</body>
+</html>
+
+
+
+
